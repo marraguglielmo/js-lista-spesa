6b85ea62a7d4c339ba830c3431b85c26d606c154
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-27 15:31:25 +0100

    [add] js

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+const lista = [
+    'pomodori',
+    'latte', 
+    'tarallini', 
+    'biscotti', 
+    'acqua', 
+    'peperoni', 
+    'carne', 
+    'birra'
+    ];
+
+    let counter = 0;
+    console.log(lista[counter]);
+    
+    while(counter < lista.length -1){
+        counter++;
+        console.log(lista[counter]);
+    }
